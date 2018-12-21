7719d4c251e758c9400d51c4f7b160616f361bd4
Fabien BEHIER <fabien.behier@icloud.com>
2018-12-21 13:00:27 +0000

    attempt highlight

--- CheekyButton.js
+++ CheekyButton.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { View, Text, StyleSheet, Dimensions, LayoutAnimation } from 'react-native'
-import { GestureHandler,  } from 'expo'
+import { View, Text, StyleSheet, Dimensions, LayoutAnimation, Animated } from 'react-native'
+import { GestureHandler } from 'expo'
+import ReactNativeHapticFeedback from 'react-native-haptic-feedback'
 
-const { PanGestureHandler, LongPressGestureHandler } = GestureHandler
+const { PanGestureHandler, TapGestureHandler } = GestureHandler
 
 const { width: windowWidth, height: windowHeight } = Dimensions.get('window')
 
-const EXPANDED_ARC_RADIUS = 125
-const COLLAPSED_BUBBLE_RADIUS = windowWidth / 10
+const EXPANDED_ARC_RADIUS =  windowWidth / 3
+const HIGHLIGTHED_ARC_RADIUS = EXPANDED_ARC_RADIUS * 1.1
+const DEADZONE_ARC_RADIUS = EXPANDED_BUBBLE_RADIUS * 2
 const EXPANDED_BUBBLE_RADIUS = windowWidth / 10
+const COLLAPSED_BUBBLE_RADIUS = windowWidth / 10
+const HIGHLIGTHED_BUBBLE_RADIUS = EXPANDED_BUBBLE_RADIUS
+const BUBBLE_SPACING = windowWidth / 50
 
 const ORIGIN = {
   top: windowHeight / 2,
@@ -26,17 +31,139 @@ class CheekyButton extends React.Component {
         label: 'B',
       }, {
         label: 'C',
+      }, {
+        label: 'D',
+      }, {
+        label: 'E',
+      }, {
+        label: 'F',
       }],
       isExpanded: false,
+      hasHighligthedBubble: false,
+      highligthedBubbleIndex: null,
+      highligthedArcRadius: EXPANDED_ARC_RADIUS
     }
   }
 
   _longPressRef = React.createRef()
+  _touchXY = new Animated.ValueXY()
+  _touchedExpandedArcAngle = new Animated.Value()
+  _touchedExpandedArcRadius = new Animated.Value()
+  _onPanGestureEvent = Animated.event([{
+    nativeEvent: {
+      translationX: this._touchXY.x,
+      translationY: this._touchXY.y,
+    }
+  }], {
+    useNativeDriver: true
+  })
 
-  _getBubbleDetails = (indice, origin) => {
+  _handleTouchXY = ({ x, y }) => {
+    const {
+      isExpanded,
+    } = this.state
+
+    const distance = Math.sqrt(x * x + y * y)
+    let angleOffset
+    let sides = (y * x) < 0 ? (
+      sides = y / x
+    ) : (
+      sides = x / y
+    )
+    if (x < 0) { { // x-
+      if (y < 0) { // y-
+        angleOffset =  Math.PI / 2
+      } else       // y+
+        angleOffset =  Math.PI
+      }
+    } else {       // x+
+      if (y < 0) { // y-
+        angleOffset = 0
+      } else {     // y-
+        angleOffset = 3 * Math.PI / 2
+      }
+    }
+
+    const angle = Math.atan(Math.abs(sides)) + angleOffset
+
+    if (isExpanded) {
+      this._touchedExpandedArcRadius.setValue(distance)
+      this._touchedExpandedArcAngle.setValue(angle)
+    }
+  }
+
+  _handleExpandedArcRadius = ({ value }) => {
+    if (value > DEADZONE_ARC_RADIUS) {
+      this.setState({
+        hasHighligthedBubble: true
+      })
+    } else {
+      this.setState({
+        hasHighligthedBubble: false,
+      })
+    }
+  }
+
+  _handleExpandedArcAngle = ({ value }) => {
     const {
       options,
+      hasHighligthedBubble,
+      highligthedBubbleIndex: currentlyHighligthedBubbleIndex,
+    } = this.state
+
+    const angleRotation = this._getAngleArcRotation()
+
+    const angleArc = this._getAngleArc()
+    const anglePerBubble = angleArc / options.length
+
+    const highligthedBubbleIndex = (Math.PI - value / anglePerBubble).toFixed(0)
+
+    console.log(highligthedBubbleIndex)
+
+    LayoutAnimation.configureNext({
+      ...LayoutAnimation.Presets.easeInEaseOut,
+      duration: 250
+    })
+
+    if (hasHighligthedBubble) {
+      if (currentlyHighligthedBubbleIndex != highligthedBubbleIndex) {
+        ReactNativeHapticFeedback.trigger('impactMedium')
+      }
+
+      this.setState({
+        highligthedArcRadius: HIGHLIGTHED_ARC_RADIUS,
+        highligthedBubbleIndex,
+      })
+    } else {
+      this.setState({
+        highligthedArcRadius: EXPANDED_ARC_RADIUS,
+        highligthedBubbleIndex: null,
+      })
+    }
+  }
+
+  _getAngleArc = () => {
+    const {
+      options: {
+        length: amountOfBubble,
+      },
+    } = this.state
+
+    return (amountOfBubble - 1) * 2 * (EXPANDED_BUBBLE_RADIUS + BUBBLE_SPACING) / EXPANDED_ARC_RADIUS
+  }
+  _getAngleArcRotation = (origin) => {
+    return 0
+  }
+
+  _getBubbleDetails = (index, origin) => {
+    const {
+      options: {
+        length: amountOfBubble,
+      },
       isExpanded,
+      hasHighligthedBubble,
+      highligthedBubbleIndex,
+      highligthedArcRadius,
     } = this.state
     
     if (!isExpanded) {
@@ -46,16 +173,21 @@ class CheekyButton extends React.Component {
         width: COLLAPSED_BUBBLE_RADIUS * 2,
       }
     } else {
-      const amountOfBubble = options.length
-      const bubbleSpace = 20
-      const angleRotation = 0
+      const angleRotation = this._getAngleArcRotation()
 
-      const angleArc = (amountOfBubble - 1) * (2 * EXPANDED_BUBBLE_RADIUS + bubbleSpace) / EXPANDED_ARC_RADIUS
-      const angleBubble = Math.PI / 2 + angleRotation + indice * angleArc / (amountOfBubble - 1) - (angleArc / 2)
+      const angleArc = this._getAngleArc()
+      const angleBubble = Math.PI / 2 + angleRotation + index * angleArc / (amountOfBubble - 1) - (angleArc / 2)
+      const isHighligthedBubble = highligthedBubbleIndex == index
+      
+      const arcRadius = isHighligthedBubble ? highligthedArcRadius : EXPANDED_ARC_RADIUS
+      const backgroundColor = isHighligthedBubble ? '#FF0000' : '#00FF00'
+      const bubbleRadius = hasHighligthedBubble ? COLLAPSED_BUBBLE_RADIUS : HIGHLIGTHED_BUBBLE_RADIUS
+
       return {
-        top: origin.top - 2 * COLLAPSED_BUBBLE_RADIUS - Math.sin(angleBubble) * EXPANDED_ARC_RADIUS,
-        left: origin.left - COLLAPSED_BUBBLE_RADIUS - Math.cos(angleBubble) * EXPANDED_ARC_RADIUS,
-        width: EXPANDED_BUBBLE_RADIUS * 2,
+        top: origin.top - 2 *bubbleRadius - Math.sin(angleBubble) * arcRadius,
+        left: origin.left - bubbleRadius - Math.cos(angleBubble) * arcRadius,
+        width: bubbleRadius * 2,
+        backgroundColor,
       }
     }
   }
@@ -67,6 +199,8 @@ class CheekyButton extends React.Component {
     })
     this.setState({
       isExpanded: true,
+    }, () => {
+      ReactNativeHapticFeedback.trigger('impactHeavy')
     })
   }
 
@@ -80,14 +214,26 @@ class CheekyButton extends React.Component {
     })
   }
 
+  componentWillMount() {
+    this._touchXY.addListener(this._handleTouchXY)
+    this._touchedExpandedArcRadius.addListener(this._handleExpandedArcRadius)
+    this._touchedExpandedArcAngle.addListener(this._handleExpandedArcAngle)
+  }
+
+  componentWillUnmount() {
+    this._touchXY.removeListener(this._handleTouchXY)
+    this._touchedExpandedArcRadius.removeListener(this._handleExpandedArcRadius)
+    this._touchedExpandedArcAngle.removeListener(this._handleExpandedArcAngle)
+  }
 
   render() {
     const {
       options,
+      hasHighligthedBubble
     } = this.state
 
     return (
-      <View>
+      <Animated.View>
         {options.map(({ label }, index) => {
           const bubbleDetails = this._getBubbleDetails(index, ORIGIN) 
           
@@ -97,7 +243,7 @@ class CheekyButton extends React.Component {
               style={[
                 styles.floatingButton, {
                 position: 'absolute',
-                ...bubbleDetails
+                ...bubbleDetails,
               }]}
             >
               <Text
@@ -108,48 +254,51 @@ class CheekyButton extends React.Component {
             </View>
           )})
         }
-        <LongPressGestureHandler
+        <TapGestureHandler
           ref={this._longPressRef}
-          onActivated={this._expand}
+          // onActivated={this._expand}
         >
-          <PanGestureHandler
-            onEnded={this._collapse}
-            simultaneousHandlers={[this._longPressRef]}
-          >
-              <View
-                  style={[
-                    styles.cheekyButton, {
-                    position: 'absolute',
-                    top: ORIGIN.top - 2 * EXPANDED_BUBBLE_RADIUS,
-                    left: ORIGIN.left - EXPANDED_BUBBLE_RADIUS,
-                  }]}
-                >  
-                  <Text
-                    style={styles.buttonText}
-                  >
-                    {'Hold me'}
-                  </Text>
-                </View>
-          </PanGestureHandler>
-        </LongPressGestureHandler>
-      </View>
+          <Animated.View>
+            <PanGestureHandler
+              onGestureEvent={this._onPanGestureEvent}
+              onBegan={this._expand}
+              onEnded={this._collapse}
+              simultaneousHandlers={[this._longPressRef]}
+            >
+                <Animated.View
+                    style={[
+                      styles.cheekyButton, {
+                      position: 'absolute',
+                      top: ORIGIN.top - 2 * EXPANDED_BUBBLE_RADIUS,
+                      left: ORIGIN.left - EXPANDED_BUBBLE_RADIUS,
+                    }]}
+                  >  
+                    <Text
+                      style={styles.buttonText}
+                    >
+                      {'Hold me'}
+                    </Text>
+                  </Animated.View>
+            </PanGestureHandler>
+          </Animated.View>
+        </TapGestureHandler>
+      </Animated.View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   cheekyButton: {
-    backgroundColor: '#FF000050',
+    backgroundColor: '#FF0000',
     aspectRatio: 1,
     width: EXPANDED_BUBBLE_RADIUS * 2,
-    borderRadius: 50,
+    borderRadius: HIGHLIGTHED_BUBBLE_RADIUS,
     justifyContent: 'center',
     alignItems: 'center',
   },
   floatingButton: {
-    backgroundColor: '#00FF00',
     aspectRatio: 1,
-    borderRadius: 50,
+    borderRadius: HIGHLIGTHED_BUBBLE_RADIUS,
     justifyContent: 'center',
     alignItems: 'center',
   },
