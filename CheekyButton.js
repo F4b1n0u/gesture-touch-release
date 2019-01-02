ceb93a02ec5cf1bcccabe76695d48ad55a459102
Fabien BEHIER <fabien.behier@icloud.com>
2019-01-02 15:57:51 +0000

    add animation on not targeted

--- CheekyButton.js
+++ CheekyButton.js
@@ -5,32 +5,36 @@ import Animated, { Easing } from 'react-native-reanimated'
 
 const {
   abs,
-  or,
   add,
   and,
   atan,
   block,
   Clock,
+  clockRunning,
+  color,
   cond,
   divide,
   eq,
   event,
-  Extrapolate,
+  greaterThan,
+  debug,
   interpolate,
   lessOrEq,
   lessThan,
+  modulo,
   multiply,
   neq,
+  not,
+  onChange,
+  or,
   set,
   sqrt,
   startClock,
   stopClock,
+  sub,
   timing,
   Value,
-  modulo,
-  greaterThan,
-  sub,
-} = Animated;
+} = Animated
 
 const { PanGestureHandler, LongPressGestureHandler, State } = GestureHandler
 
@@ -44,6 +48,8 @@ const EXPANDED_ARC_RADIUS = WINDOW_WIDTH / 4
 const HIGHLIGTHED_ARC_RADIUS = 1.4
 const HIGHLIGTHED_SCALE = 1.4
 
+const SELECTION_RATIO = 1.5
+const DESELECTION_RATIO = 2
 
 const LONG_PRESS_DURATION = 250
 const LONG_PRESS_MAX_DIST = 20
@@ -61,12 +67,12 @@ function runExpansion(longPressState) {
     position: new Value(0),
     frameTime: new Value(0),
     time: new Value(0),
-  };
+  }
 
   const config = {
     toValue: new Value(0),
     duration: new Value(125),
-    easing: Easing.inOut(Easing.ease),
+    easing: Easing.in(Easing.elastic(1)),
   }
 
   return block([
@@ -75,6 +81,7 @@ function runExpansion(longPressState) {
       set(state.time, 0),
       set(state.frameTime, 0),
       set(config.toValue, 1),
+      set(config.duration, new Value(300)),
       startClock(clock),
     ]),
     cond(and(eq(longPressState, State.END), neq(config.toValue, 0)), [
@@ -82,12 +89,64 @@ function runExpansion(longPressState) {
       set(state.time, 0),
       set(state.frameTime, 0),
       set(config.toValue, 0),
+      set(config.duration, new Value(125)),
       startClock(clock),
     ]),
     timing(clock, state, config),
     cond(state.finished, stopClock(clock)),
     state.position,
   ])
+}
+
+function runDistance(isTargeted, { x: touchX, y: touchY }) {
+  const clock = new Clock()
+  
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    frameTime: new Value(0),
+    time: new Value(0),
+  }
+
+
+  // TODO need to check why there is this bug when you go behind and pass change isTarget
+  const config = {
+    toValue: new Value(0),
+    duration: new Value(50),
+    easing: Easing.inOut(Easing.ease),
+  }
+
+  const distance = cond(isTargeted,
+    sqrt(add(multiply(touchX, touchX), multiply(touchY, touchY))),
+    0
+  )
+  
+  return block([
+    onChange(isTargeted,
+      cond(neq(config.toValue, distance), [
+        set(state.finished, 0),
+        set(state.time, 0),
+        set(state.frameTime, 0),
+        set(config.toValue, distance),
+        startClock(clock),
+      ]),
+    ),
+
+    timing(clock, state, config),
+    cond(state.finished, stopClock(clock)),
+
+    cond(clockRunning(clock),
+      state.position,
+      distance,
+    )
+  ])
+}
+
+function runBackgroundColor(isSelected) {
+  return cond(isSelected,
+    color(255, 0, 0, .5),
+    color(255, 0, 0, 1)
+  )
 }
 
 class CheekyButton extends React.Component {
@@ -99,15 +158,15 @@ class CheekyButton extends React.Component {
       {
         label: 'A',
       },
-      {
-        label: 'B',
-      },
-      {
-        label: 'C',
-      },
-      {
-        label: 'D',
-      },
+      // {
+      //   label: 'B',
+      // },
+      // {
+      //   label: 'C',
+      // },
+      // {
+      //   label: 'D',
+      // },
     ]}
   }
 
@@ -144,8 +203,15 @@ class CheekyButton extends React.Component {
             radius={BUBBLE_RADIUS}
             position={position}
             touch={{
-              x: this._touchX,
-              y: this._touchY,
+              x: block([
+                // to avoid to keep the last position after released the finger
+                cond(neq(this._longPressState, State.ACTIVE), set(this._touchX, 0)),
+                this._touchX,
+              ]),
+              y: block([
+                cond(neq(this._longPressState, State.ACTIVE), set(this._touchY, 0)),
+                this._touchY,
+              ]),
             }}
             bubbleQuantity={options.length}
             {...bubble}
@@ -207,11 +273,14 @@ class Bubble extends React.Component {
     super(props)
 
     const {
-      touch: {
-        x: touchX,
-        y: touchY,
-      }
+      touch,
+      position,
     } = props
+    
+    const {
+      x: touchX,
+      y: touchY,
+    } = touch
 
     const offsetAngle = this._getOffsetAngle()
     const cos = + Math.cos(offsetAngle)
@@ -220,33 +289,13 @@ class Bubble extends React.Component {
       props.expansion, {
       inputRange:  [0, 1],
       outputRange: [0, EXPANDED_ARC_RADIUS],
-      easing: Easing.inOut(Easing.linear),
-      extrapolate: Extrapolate.clamp,
+      extrapolateLeft: 'clamp',
     })
     const expandX = multiply(cos, expansionDistance)
     const expandY = multiply(sin, expansionDistance)
   
     const isExpanded = greaterThan(props.expansion, 0)
-    const touchDistance = cond(isExpanded,
-      sqrt(add(multiply(touchX, touchX), multiply(touchY, touchY))),
-      // TODO check why it is not reseted to 0
-      0
-    )
-    const highligtedTranslateRatio = interpolate(
-      touchDistance, {
-      inputRange:  [0, EXPANDED_ARC_RADIUS, 2 * EXPANDED_ARC_RADIUS],
-      outputRange: [1, HIGHLIGTHED_ARC_RADIUS, 1],
-      easing: Easing.inOut(Easing.linear),
-      extrapolate: Extrapolate.clamp,
-    })
-    const highligtedScaleRatio = interpolate(
-      touchDistance, {
-      inputRange:  [0, EXPANDED_ARC_RADIUS, 2 * EXPANDED_ARC_RADIUS],
-      outputRange: [1, HIGHLIGTHED_SCALE, 1],
-      easing: Easing.inOut(Easing.linear),
-      extrapolate: Extrapolate.clamp,
-    })
-   
+  
     const angleOffset = new Value(0)
     const sides = new Value(0)
     const targetedAngle = block([
@@ -272,13 +321,6 @@ class Bubble extends React.Component {
       ])
     ])
     
-    //     from: + angle / 2,
-    //     to: - angle / 2,
-    //     angle: relativeAngle,
-    
-    //   var _from  = from  % (2 * Math.PI),
-    //       _to    = to    % (2 * Math.PI),
-    //       _angle = angle % (2 * Math.PI);
     const angle = this._getAngle()
     const minAngle = (+ angle / 2) % (2 * Math.PI)
     const maxAngle = (- angle / 2) % (2 * Math.PI)
@@ -287,27 +329,14 @@ class Bubble extends React.Component {
     const relativeAngle = modulo(sub(targetedAngle, offsetAngle), (2 * Math.PI))
 
     const isInRange = block([
-      //   if (_from  < 0) _from  += (2 * Math.PI); // (-500) % (2 * Math.PI) === -140 :(
-      //   if (_to    < 0) _to    += (2 * Math.PI);
-      //   if (_angle < 0) _angle += (2 * Math.PI);
       cond(lessThan(from, 0),           set(from,           add(from, (2 * Math.PI)))),
       cond(lessThan(to, 0),             set(to,             add(to, (2 * Math.PI)))),
       cond(lessThan(relativeAngle, 0),  set(relativeAngle,  add(relativeAngle, (2 * Math.PI)))),
-    
-      //   if (_from === _to) {
-      //     if (to > from)
-      //       return true; // whole circle
-      //     return _angle === _from; // exact only
-      //   } else {
-      //     if (_to < _from)
-      //       return _angle <= _to || from <= _angle; // _angle outside range
-      //     return _from <= _angle && _angle <= _to; 
-      //   }
 
       cond(eq(from, to),
         [
           cond(greaterThan(to, from),
-            true,
+            1,
           ),
           eq(relativeAngle, from),
         ],
@@ -325,45 +354,57 @@ class Bubble extends React.Component {
       ),
     ])
 
-    //   if (isInRange({
+    const isTargeted = and(isExpanded, isInRange)
     
-    //   }) && isExpanded) { // not targeted
-    //     if (isTargeted) {
-    //       this.setState({
-    //         isTargeted: false,
-    //       })
-    //     }
-    //   } else { // targeted
-    //     if (!isTargeted) {
-    //       this.setState({
-    //         isTargeted: true,
-    //       })
-    //     }
-    //   }
-    // }
+    const touchDistance = runDistance(isTargeted, touch)
 
-    const isTargeted = cond(and(isExpanded, isInRange),
-      cond(this.targetedState,
-        false,
-        true,
-      ),
-      false,
+    const highligtedTranslateRatio = interpolate(
+      touchDistance, {
+      inputRange:  [0, EXPANDED_ARC_RADIUS, 2 * EXPANDED_ARC_RADIUS],
+      outputRange: [1, HIGHLIGTHED_ARC_RADIUS, 1],
+      extrapolateRight: 'clamp',
+    })
+    const highligtedScaleRatio = interpolate(
+      touchDistance, {
+      inputRange:  [0, EXPANDED_ARC_RADIUS, 2 * EXPANDED_ARC_RADIUS],
+      outputRange: [1, HIGHLIGTHED_SCALE, 1],
+      extrapolateRight: 'clamp',
+    })
+
+    this._translateX = multiply(expandX, highligtedTranslateRatio)
+
+    this._translateY = multiply(expandY, highligtedTranslateRatio)
+
+    this._scale = highligtedScaleRatio
+
+    const deltaX = sub(expandX, touchX)
+    const deltaY = sub(expandY, touchY)
+    
+    let range = debug('range',
+      sqrt(add(multiply(deltaX, deltaX), multiply(deltaY, deltaY)))
+    )
+    let isInSelectionRange = debug('isInSelectionRange',
+      lessThan(range, SELECTION_RATIO * BUBBLE_RADIUS)
+    )
+    let isInDeselectionRange = debug('isInDeselectionRange',
+      greaterThan(range, DESELECTION_RATIO * BUBBLE_RADIUS)
     )
 
-    this._translateX = cond(isTargeted,
-      multiply(expandX, highligtedTranslateRatio),
-      expandX,
+    let isSelected = debug('isSelected',
+      cond(isTargeted,
+        [
+          cond(isInSelectionRange,
+            1,
+          ),
+          cond(isInDeselectionRange,
+            0,
+          )
+        ],
+      0,
+      )
     )
 
-    this._translateY = cond(isTargeted,
-      multiply(expandY, highligtedTranslateRatio),
-      expandY,
-    )
-
-    this._scale = cond(isTargeted,
-      highligtedScaleRatio,
-      1
-    )
+    this._backgroundColor = runBackgroundColor(isSelected)
   }
 
   _getAngleRotation = () => {
@@ -405,7 +446,7 @@ class Bubble extends React.Component {
     if (forcedBackgroundColor) {
       backgroundColor = forcedBackgroundColor
     } else {
-      backgroundColor = '#ff0000'
+      backgroundColor = this._backgroundColor
     }
 
     return (
